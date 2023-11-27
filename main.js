5aa1f4c8cff30b2b082493fa18597597a17afa54
webglBuilds <joyjack12@hotmail.com>
2023-11-27 16:18:04 +0200

    init

--- main.js
+++
@@ -1,6 +0,0 @@
-import './style.css'
-import './sassStyles.scss'
-import Experience from './Experience/Experience'
-
-const experience = new Experience(document.querySelector(".experience-canvas"))
-
